33a44bc2223bd1e3ac685b4ae36b66ed1561f0b3
NgsTks <ngstks.h113fd@gmail.com>
2026-06-13 11:04:01 +0900

    /presentの改良

--- main.py
+++ main.py
@@ -92,4 +92,7 @@ def index():
 
 @app.post("/present")
 async def give_present(present):
-    return {"response": f"サーバです。メリークリスマス！ {present}ありがとう。お返しはキャンディーです。"}  # f文字列というPythonの機能を使っている
+    if (len(present) > 1):
+        return {"response": f"私は{present}の妖精だ{present[-2:]}ー！"} 
+    else:
+        return {"response": f"{present}ー！"}
